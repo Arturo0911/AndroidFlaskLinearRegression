f17c5c142ffba45e13f08c1701112421529916af
Arturon <anegreiross@outlook.com>
2020-12-28 15:38:02 -0500

    setted mongodb with her URI

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask import jsonify
-
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Meteorology'
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello_world():
     return "Hello world"
 
-@app.route("/meteor")
+@app.route("/meteor", methods=['GET'])
 def meteor():
     return jsonify({"latitude":"-2.335017", "longitude":'-80.229769'})
 
--- manage.py
+++
@@ -1,1 +0,0 @@
-from flask-script import manage
