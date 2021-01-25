56bb9c7bbd78491baefd0a082ddd896c6868ca40
Arturon <anegreiross@outlook.com>
2021-01-24 20:45:02 -0500

    added bars

--- android/client-android/app/src/main/java/com/example/puremango/LoginPage.java
+++ android/client-android/app/src/main/java/com/example/puremango/LoginPage.java
@@ -2,13 +2,19 @@ package com.example.puremango;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-
+import androidx.appcompat.app.ActionBar;
 public class LoginPage extends AppCompatActivity {
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        actionBar  = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#273036")));
+        actionBar.setTitle("Pure Mango");
     }
 }
