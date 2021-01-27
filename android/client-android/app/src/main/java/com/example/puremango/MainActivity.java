1a71d47c53d6c415687dbaa4761fd45a0935c549
Arturon <anegreiross@outlook.com>
2021-01-26 19:08:31 -0500

    ok

--- android/client-android/app/src/main/java/com/example/puremango/MainActivity.java
+++ android/client-android/app/src/main/java/com/example/puremango/MainActivity.java
@@ -3,8 +3,48 @@ package com.example.puremango;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String postRquest = "http://" + "10.0.2.2"+":"+5000+"/graphql";
+
+    public void onButtonTestClick(View view){
+        try {
+            ApolloClient apolloClient = ApolloClient.builder()
+                    .serverUrl(postRquest)
+                    .build();
+
+            apolloClient.query(new QueryEmployeeQuery())
+                    .enqueue(new ApolloCall.Callback<QueryEmployeeQuery.Data>() {
+
+                        @Override
+                        public void onResponse(@NotNull Response<QueryEmployeeQuery.Data> response) {
+                            Log.i("on response: ", "onResponse: "+response.getData().allEmployee.edges.get(0).node.credentials());
+                            //Toast.makeText(MainActivity.this, response.getData().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                        @Override
+                        public void onFailure(@NotNull ApolloException e) {
+                            e.printStackTrace();
+                            //Toast.makeText(MainActivity.this, "Error in: "+e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }catch (Exception e){
+            Toast.makeText(this, "Error by: "+e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 
     //private
 
