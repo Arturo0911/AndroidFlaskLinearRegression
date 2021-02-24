d8834f32e70b2e2660060255bba407cffdc517dd
Arturo Negreiros <anegreiross@outlook.com>
2021-02-24 15:08:47 -0500

    ok

--- Authentication/src/main/java/com/microservice/authentication/Authentication/AuthenticationApplication.java
+++ Authentication/src/main/java/com/microservice/authentication/Authentication/AuthenticationApplication.java
@@ -3,6 +3,10 @@ package com.microservice.authentication.Authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Arthur payload
+ */
+
 @SpringBootApplication
 public class AuthenticationApplication {
 
